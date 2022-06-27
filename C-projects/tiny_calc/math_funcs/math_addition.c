bf314e8dbbe3eeca6fdca495ed420bd6d9f0875e
RaSi96 <102865950+RaSi96@users.noreply.github.com>
2022-06-27 19:38:30 +0530

    Added calculator math functions directory

---
+++ C-projects/tiny_calc/math_funcs/math_addition.c
@@ -0,0 +1,10 @@
+# include "../src/claytor.h"
+
+int32_t op_add(int32_t augend, int32_t addend)
+{
+	/* Addition function used by the calculator. It adds operand_2 (the addend)
+	 * to operand_1 (the augend.)
+	 */
+	int32_t sum = augend + addend;
+	return (sum);
+} //end int16_t addition()
