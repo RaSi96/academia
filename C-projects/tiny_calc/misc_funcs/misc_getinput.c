1b533fbb721dfd54dc3f900594725698c7ef1b31
RaSi96 <102865950+RaSi96@users.noreply.github.com>
2022-06-27 19:36:58 +0530

    Added calculator misc functions directory

---
+++ C-projects/tiny_calc/misc_funcs/misc_getinput.c
@@ -0,0 +1,21 @@
+# include "../src/claytor.h"
+
+char *get_input(char *dest_array, int n)
+{
+	/* This function exists as a wrapper to fgets(): setting the input buffer to
+	 * stdin automatically and also parsing out the newline terminator that is
+	 * read in by fgets after the stream from stdin has ended. If for whatever
+	 * reason stdin is inaccessible, it will report an error and exit outright.
+	 */
+	setbuf(stdin, NULL);
+	if (fgets(dest_array, n, stdin))
+	{
+		dest_array[strcspn(dest_array, "\n")] = 0;
+		return dest_array;
+	}
+	else
+	{
+		fprintf(stderr, "get_input(): Error reading from stdin.\n");
+		exit(EXIT_FAILURE);
+	}
+} //end char *get_input()
